776d7269aaa962336ec52988dc82c3975e82643b
ivanparam <ivanp.phustech@gmail.com>
2026-02-11 14:51:55 +0300

    add s

---
+++ double_push.py
@@ -0,0 +1,31 @@
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setmode(GPIO.BCM)
+leds = [16, 12, 25, 17, 27, 23, 22, 24]
+GPIO.setup(leds, GPIO.OUT)
+GPIO.output(leds, 0)
+sleep_time = 0.2
+up = 9
+down = 10
+num = 0
+GPIO.setup(up, GPIO.IN)
+GPIO.setup(down, GPIO.IN)
+def dec2bin(value):
+    return [int(element) for element in bin(value)[2:].zfill(8)]
+while True:
+    if GPIO.input(up) and not GPIO.input(down):
+        num = num+1
+        if num < 0 or num > 255:
+            num = 0
+        print(num, dec2bin(num))
+        time.sleep(sleep_time)
+    if GPIO.input(down) and not GPIO.input(up):
+        num = num-1
+        if num < 0 or num > 255:
+            num = 255
+        print(num, dec2bin(num))
+        time.sleep(sleep_time)    
+    if GPIO.input(down) and GPIO.input(up):
+        num = 255
+    GPIO.output(leds, dec2bin(num))
